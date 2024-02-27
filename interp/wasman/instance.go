f286aa5de237684e6843514696e40d61546a0b85
deadprogram <ron@hybridgroup.com>
2024-02-27 20:43:24 +0100

    interp/wasman: add the other wasm types as valid param types for calling instance
    Signed-off-by: deadprogram <ron@hybridgroup.com>

--- interp/wasman/instance.go
+++ interp/wasman/instance.go
@@ -27,6 +27,22 @@ func (i *Instance) Call(name string, args ...interface{}) (interface{}, error) {
 		for i, v := range args {
 			wargs[i] = uint64(v.(uint32))
 		}
+	case int64:
+		for i, v := range args {
+			wargs[i] = uint64(v.(int64))
+		}
+	case uint64:
+		for i, v := range args {
+			wargs[i] = uint64(v.(uint64))
+		}
+	case float32:
+		for i, v := range args {
+			wargs[i] = uint64(v.(float32))
+		}
+	case float64:
+		for i, v := range args {
+			wargs[i] = uint64(v.(float64))
+		}
 	}
 	results, _, err := i.instance.CallExportedFunc(name, wargs...)
 	if err != nil {
