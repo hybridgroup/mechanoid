27c6d9d9fde44e36ddf636b860700d9248ebc1b7
deadprogram <ron@hybridgroup.com>
2024-02-27 23:58:36 +0100

    engine, interp/wasman: handle the 4 basic value types correctly when calling wasman
    Signed-off-by: deadprogram <ron@hybridgroup.com>

--- interp/wasman/instance.go
+++ interp/wasman/instance.go
@@ -1,53 +1,92 @@
 package wasman
 
 import (
+	"math"
+
 	wasmaneng "github.com/hybridgroup/wasman"
+	"github.com/hybridgroup/wasman/types"
 )
 
 type Instance struct {
 	instance *wasmaneng.Instance
 }
 
-func (i *Instance) Call(name string, args ...interface{}) (interface{}, error) {
+func (i *Instance) Call(name string, args ...any) (any, error) {
 	if len(args) == 0 {
-		results, _, err := i.instance.CallExportedFunc(name)
+		results, types, err := i.instance.CallExportedFunc(name)
 		if err != nil {
 			return nil, err
 		}
-		return results, nil
+		res := decodeResults(results, types)
+		if len(res) == 0 {
+			return nil, nil
+		}
+		if len(res) == 1 {
+			return res[0], nil
+		}
+		return res, nil
 	}
 
-	wargs := make([]uint64, len(args))
-	switch args[0].(type) {
-	case int32:
-		for i, v := range args {
-			wargs[i] = uint64(v.(int32))
-		}
-	case uint32:
-		for i, v := range args {
-			wargs[i] = uint64(v.(uint32))
-		}
-	case int64:
-		for i, v := range args {
-			wargs[i] = uint64(v.(int64))
-		}
-	case uint64:
-		for i, v := range args {
-			wargs[i] = uint64(v.(uint64))
-		}
-	case float32:
-		for i, v := range args {
-			wargs[i] = uint64(v.(float32))
-		}
-	case float64:
-		for i, v := range args {
-			wargs[i] = uint64(v.(float64))
-		}
-	}
-	results, _, err := i.instance.CallExportedFunc(name, wargs...)
+	results, types, err := i.instance.CallExportedFunc(name, encodeArgs(args)...)
 	if err != nil {
 		return nil, err
 	}
 
-	return results, nil
+	res := decodeResults(results, types)
+	if len(res) == 0 {
+		return nil, nil
+	}
+	if len(res) == 1 {
+		return res[0], nil
+	}
+	return res, nil
+}
+
+func encodeArgs(args []any) []uint64 {
+	encoded := make([]uint64, 0, len(args))
+	for _, arg := range args {
+		encoded = append(encoded, encodeArg(arg))
+	}
+	return encoded
+}
+
+func encodeArg(arg any) uint64 {
+	switch val := arg.(type) {
+	case int32:
+		return uint64(val)
+	case int64:
+		return uint64(val)
+	case float32:
+		return uint64(math.Float32bits(val))
+	case float64:
+		return uint64(math.Float64bits(val))
+	case uint32:
+		return uint64(val)
+	case uint64:
+		return uint64(val)
+	}
+	panic("bad arg type")
+}
+
+func decodeResults(results []uint64, vtypes []types.ValueType) []any {
+	decoded := make([]any, 0, len(results))
+	for i, result := range results {
+		vtype := vtypes[i]
+		decoded = append(decoded, decodeResult(result, vtype))
+	}
+	return decoded
+}
+
+func decodeResult(result uint64, vtype types.ValueType) any {
+	switch vtype {
+	case types.ValueTypeF32:
+		return math.Float32frombits(uint32(result))
+	case types.ValueTypeF64:
+		return math.Float64frombits(uint64(result))
+	case types.ValueTypeI32:
+		return int32(result)
+	case types.ValueTypeI64:
+		return int64(result)
+	}
+	panic("unreachable")
 }
