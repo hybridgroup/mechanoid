08b2935d91558638d7e5bbe307e8354fc83a06ca
deadprogram <ron@hybridgroup.com>
2024-02-29 13:48:30 +0100

    cmd/mecha: improve output from mecha flash command and simplify the code
    Signed-off-by: deadprogram <ron@hybridgroup.com>

--- cmd/mecha/flash.go
+++ cmd/mecha/flash.go
@@ -1,10 +1,11 @@
 package main
 
 import (
-	"bufio"
 	"bytes"
 	"fmt"
+	"io"
 	"log"
+	"os"
 	"os/exec"
 
 	"github.com/urfave/cli/v2"
@@ -15,45 +16,31 @@ func flash(cCtx *cli.Context) error {
 		return fmt.Errorf("target board required")
 	}
 
+	targetName := cCtx.Args().First()
+
+	var cmd *exec.Cmd
 	if cCtx.Bool("monitor") {
-		return flashAndMonitor(cCtx)
+		cmd = exec.Command("tinygo", "flash", "-size", "short", "-target", targetName, "-monitor", ".")
+	} else {
+		cmd = exec.Command("tinygo", "flash", "-size", "short", "-target", targetName, ".")
 	}
 
-	targetName := cCtx.Args().First()
+	var stdoutBuf, stderrBuf bytes.Buffer
+	cmd.Stdout = io.MultiWriter(os.Stdout, &stdoutBuf)
+	cmd.Stderr = io.MultiWriter(os.Stderr, &stderrBuf)
 
-	var stdout, stderr bytes.Buffer
-	cmd := exec.Command("tinygo", "flash", "-size", "short", "-target", targetName, ".")
-	cmd.Stdout = &stdout
-	cmd.Stderr = &stderr
 	if err := cmd.Run(); err != nil {
-		log.Fatalf("tinygo flash -size short -target %s .: %v\n%s%s", targetName, err, stderr.Bytes(), stdout.Bytes())
-	}
-
-	fmt.Println(stdout.String())
-
-	return nil
-}
-
-func flashAndMonitor(cCtx *cli.Context) error {
-	targetName := cCtx.Args().First()
-
-	var stderr bytes.Buffer
-	cmd := exec.Command("tinygo", "flash", "-size", "short", "-target", targetName, "-monitor", ".")
-	cmd.Stderr = &stderr
-	stdout, err := cmd.StdoutPipe()
-	if err != nil {
-		return err
-	}
-
-	if err := cmd.Start(); err != nil {
-		log.Fatalf("tinygo flash -size short -target %s .: %v\n%s", targetName, err, stderr.Bytes())
+		log.Fatalf("tinygo flash -size short -target %s .: %v\n", targetName, err) //, stderr.Bytes())
 	}
 
 	// print the monitoring output
-	scanner := bufio.NewScanner(stdout)
-	for scanner.Scan() {
-		m := scanner.Text()
-		fmt.Println(m)
+	if cCtx.Bool("monitor") {
+		outStr, errStr := stdoutBuf.String(), stderrBuf.String()
+		if errStr != "" {
+			fmt.Println(errStr)
+		} else {
+			fmt.Println(outStr)
+		}
 	}
 
 	return nil
