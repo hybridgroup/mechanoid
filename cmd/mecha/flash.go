cc2fcda58915dcf3b836366a35da7458653577e8
deadprogram <ron@hybridgroup.com>
2024-03-01 10:01:11 +0100

    cmd/mecha: add stack-size=8kb as default for flashing boards
    Signed-off-by: deadprogram <ron@hybridgroup.com>

--- cmd/mecha/flash.go
+++ cmd/mecha/flash.go
@@ -20,9 +20,9 @@ func flash(cCtx *cli.Context) error {
 
 	var cmd *exec.Cmd
 	if cCtx.Bool("monitor") {
-		cmd = exec.Command("tinygo", "flash", "-size", "short", "-target", targetName, "-monitor", ".")
+		cmd = exec.Command("tinygo", "flash", "-size", "short", "-stack-size", "8kb", "-target", targetName, "-monitor", ".")
 	} else {
-		cmd = exec.Command("tinygo", "flash", "-size", "short", "-target", targetName, ".")
+		cmd = exec.Command("tinygo", "flash", "-size", "short", "-stack-size", "8kb", "-target", targetName, ".")
 	}
 
 	var stdoutBuf, stderrBuf bytes.Buffer
@@ -30,7 +30,7 @@ func flash(cCtx *cli.Context) error {
 	cmd.Stderr = io.MultiWriter(os.Stderr, &stderrBuf)
 
 	if err := cmd.Run(); err != nil {
-		log.Fatalf("tinygo flash -size short -target %s .: %v\n", targetName, err) //, stderr.Bytes())
+		log.Fatalf("tinygo flash -size short -stack-size 8kb -target %s .: %v\n", targetName, err) //, stderr.Bytes())
 	}
 
 	// print the monitoring output
