b215ed0d7e60ed2f01f082ea6159170c808e0e20
deadprogram <ron@hybridgroup.com>
2024-03-08 17:12:48 +0100

    log: add DebugMemory() logger
    Signed-off-by: deadprogram <ron@hybridgroup.com>

--- log.go
+++ log.go
@@ -1,5 +1,7 @@
 package mechanoid
 
+import "runtime"
+
 // Log a message into terminal.
 func Log(msg string) {
 	println(msg)
@@ -9,5 +11,14 @@ func Log(msg string) {
 func Debug(args ...any) {
 	if Debugging {
 		println(args)
+	}
+}
+
+// DebugMemory prints memory usage if debug mode is enabled.
+func DebugMemory(args ...any) {
+	if Debugging {
+		ms := runtime.MemStats{}
+		runtime.ReadMemStats(&ms)
+		println(args, "Heap Used: ", ms.HeapInuse, " Free: ", ms.HeapIdle, " Meta: ", ms.GCSys)
 	}
 }
